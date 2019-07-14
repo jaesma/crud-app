8d95e4ace7a55912dadcfad0be029bbc8a140188
jaesma <jamesmanalang11392@gmail.com>
2019-07-14 17:46:26 +0800

    Second Commit. Adding Product Repository.

---
+++ src/main/java/com/manalangjames/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.manalangjames.repository;
+
+import com.manalangjames.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
